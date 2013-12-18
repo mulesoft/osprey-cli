07dce7fad6ccf2cfc1904d5355671fc4b8fca4ad
Javier Centurion <javier.centurion@southworks.net>
2013-12-18 00:49:43 -0300

    add uri to template generator

--- examples/end-solution/lib/app.js
+++ examples/end-solution/lib/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var ApiKitRouter, apiKit, app, express, http, parser, path,
+  var ApiKitRouter, apiKit, app, express, http, parser, path, utils,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   express = require('express');
@@ -10,31 +10,70 @@
 
   parser = require('../../../dist/toolkit-parser');
 
+  utils = require('express/lib/utils');
+
   ApiKitRouter = (function() {
     function ApiKitRouter(context, resources) {
       this.context = context;
       this.resources = resources;
       this.routerExists = __bind(this.routerExists, this);
       this.methodLookup = __bind(this.methodLookup, this);
+      this.defaultDeleteRoute = __bind(this.defaultDeleteRoute, this);
+      this.defaultPutRoute = __bind(this.defaultPutRoute, this);
+      this.defaultPostRoute = __bind(this.defaultPostRoute, this);
       this.defaultGetRoute = __bind(this.defaultGetRoute, this);
     }
 
-    ApiKitRouter.prototype.defaultGetRoute = function(res, uri) {
+    ApiKitRouter.prototype.defaultGetRoute = function(res, uri, template) {
       var methodInfo, _ref, _ref1, _ref2, _ref3;
       if (!this.routerExists('get', uri)) {
-        methodInfo = this.methodLookup('get', uri);
+        methodInfo = this.methodLookup('get', template);
         if (methodInfo != null) {
-          return res.send((_ref = methodInfo.responses) != null ? (_ref1 = _ref['200']) != null ? (_ref2 = _ref1.body) != null ? (_ref3 = _ref2['application/json']) != null ? _ref3.example : void 0 : void 0 : void 0 : void 0);
+          res.contentType('application/json');
+          return res.send((_ref = methodInfo.responses) != null ? (_ref1 = _ref['200']) != null ? (_ref2 = _ref1.body) != null ? (_ref3 = _ref2['application/json']) != null ? _ref3.example : void 0 : void 0 : void 0 : void 0, {
+            'Content-Type': 'application/json'
+          }, 200);
+        }
+      }
+    };
+
+    ApiKitRouter.prototype.defaultPostRoute = function(res, uri, template) {
+      var methodInfo;
+      if (!this.routerExists('post', uri)) {
+        methodInfo = this.methodLookup('post', template);
+        if (methodInfo != null) {
+          res.contentType('application/json');
+          return res.send(201);
+        }
+      }
+    };
+
+    ApiKitRouter.prototype.defaultPutRoute = function(res, uri, template) {
+      var methodInfo;
+      if (!this.routerExists('put', uri)) {
+        methodInfo = this.methodLookup('put', template);
+        if (methodInfo != null) {
+          return res.send(204);
+        }
+      }
+    };
+
+    ApiKitRouter.prototype.defaultDeleteRoute = function(res, uri, template) {
+      var methodInfo;
+      if (!this.routerExists('delete', uri)) {
+        methodInfo = this.methodLookup('delete', template);
+        if (methodInfo != null) {
+          return res.send(204);
         }
       }
     };
 
     ApiKitRouter.prototype.methodLookup = function(httpMethod, uri) {
-      var methodInfo, _ref, _ref1;
+      var methodInfo, _ref;
       if (((_ref = this.resources[uri]) != null ? _ref.methods : void 0) != null) {
-        methodInfo = (_ref1 = this.resources[uri]) != null ? _ref1.methods.filter(function(method) {
+        methodInfo = this.resources[uri].methods.filter(function(method) {
           return method.method === httpMethod;
-        }) : void 0;
+        });
       }
       if ((methodInfo != null) && methodInfo.length) {
         return methodInfo[0];
@@ -45,11 +84,13 @@
 
     ApiKitRouter.prototype.routerExists = function(httpMethod, uri) {
       var result;
-      result = this.context.routes[httpMethod].filter(function(route) {
-        var _ref;
-        return (_ref = uri.match(route.regexp)) != null ? _ref.length : void 0;
-      });
-      return result.length === 1;
+      if (this.context.routes[httpMethod] != null) {
+        result = this.context.routes[httpMethod].filter(function(route) {
+          var _ref;
+          return (_ref = uri.match(route.regexp)) != null ? _ref.length : void 0;
+        });
+      }
+      return (result != null) && result.length === 1;
     };
 
     return ApiKitRouter;
@@ -59,11 +100,36 @@
   apiKit = function(raml) {
     return function(req, res, next) {
       return parser.loadRaml(raml, function(toolkitParser) {
-        var resources, router;
+        var resources, router, template, uriTemplates, _fn, _i, _len;
         resources = toolkitParser.getResources();
-        router = new ApiKitRouter(app, resources);
-        if (req.method === 'GET') {
-          router.defaultGetRoute(res, req.url);
+        uriTemplates = toolkitParser.getUriTemplates();
+        _fn = function(template) {
+          var regexp;
+          regexp = utils.pathRegexp(template.uriTemplate, [], false, false);
+          return template.regexp = regexp;
+        };
+        for (_i = 0, _len = uriTemplates.length; _i < _len; _i++) {
+          template = uriTemplates[_i];
+          _fn(template);
+        }
+        template = uriTemplates.filter(function(template) {
+          var _ref;
+          return (_ref = req.url.match(template.regexp)) != null ? _ref.length : void 0;
+        });
+        if (template.length) {
+          router = new ApiKitRouter(app, resources);
+          if (req.method === 'GET') {
+            router.defaultGetRoute(res, req.url, template[0].uriTemplate);
+          }
+          if (req.method === 'POST') {
+            router.defaultPostRoute(res, req.url, template[0].uriTemplate);
+          }
+          if (req.method === 'PUT') {
+            router.defaultPutRoute(res, req.url, template[0].uriTemplate);
+          }
+          if (req.method === 'DELETE') {
+            router.defaultDeleteRoute(res, req.url, template[0].uriTemplate);
+          }
         }
         return next();
       });
@@ -76,27 +142,11 @@
 
   app.use(express.logger('dev'));
 
-  app.use(express.json());
-
   app.use(express.bodyParser());
 
   app.use(express.methodOverride());
 
   app.use(apiKit('../leagues/leagues.raml'));
-
-  app.get('/teams', function(req, res) {
-    return res.send([
-      {
-        name: 'All Teams'
-      }
-    ]);
-  });
-
-  app.get('/teams/:id', function(req, res) {
-    return res.send({
-      name: 'by Id'
-    });
-  });
 
   http.createServer(app).listen(app.get('port'), function() {
     return console.log('Express server listening on port ' + app.get('port'));
--- examples/end-solution/src/app.coffee
+++ examples/end-solution/src/app.coffee
@@ -2,39 +2,89 @@ express = require 'express'
 http = require 'http'
 path = require 'path'
 parser = require '../../../dist/toolkit-parser'
+utils = require 'express/lib/utils'
 
 class ApiKitRouter
   constructor: (@context, @resources) ->
 
-  defaultGetRoute: (res, uri) =>
+  defaultGetRoute: (res, uri, template) =>
     unless @routerExists 'get', uri
-      methodInfo = @methodLookup 'get', uri
+      methodInfo = @methodLookup 'get', template
 
       if methodInfo?
-        res.send methodInfo.responses?['200']?.body?['application/json']?.example
+        # TODO: Add validations
+        # TODO: Add content negociation
+        res.contentType('application/json');
+        res.send methodInfo.responses?['200']?.body?['application/json']?.example, { 'Content-Type': 'application/json' }, 200
+
+  defaultPostRoute: (res, uri, template) =>
+    unless @routerExists 'post', uri
+      methodInfo = @methodLookup 'post', template
+
+      if methodInfo?
+        # TODO: Add validations
+        # TODO: Add content negociation
+        res.contentType('application/json');
+        res.send 201
+
+  defaultPutRoute: (res, uri, template) =>
+    unless @routerExists 'put', uri
+      methodInfo = @methodLookup 'put', template
+
+      if methodInfo?
+        # TODO: Add validations
+        res.send 204
+
+  defaultDeleteRoute: (res, uri, template) =>
+    unless @routerExists 'delete', uri
+      methodInfo = @methodLookup 'delete', template
+
+      if methodInfo?
+        # TODO: Add validations
+        res.send 204
 
   methodLookup: (httpMethod, uri) =>
     if @resources[uri]?.methods?
-      methodInfo = @resources[uri]?.methods.filter (method) ->
+      methodInfo = @resources[uri].methods.filter (method) ->
         method.method == httpMethod
 
     if methodInfo? and methodInfo.length then methodInfo[0] else null
 
   routerExists: (httpMethod, uri) =>
-    result = @context.routes[httpMethod].filter (route) ->
-      uri.match(route.regexp)?.length
+    if @context.routes[httpMethod]?
+      result = @context.routes[httpMethod].filter (route) ->
+        uri.match(route.regexp)?.length
 
-    result.length is 1
+    result? and result.length is 1
 
 apiKit = (raml) ->
   (req, res, next) ->
     parser.loadRaml raml, (toolkitParser) ->
       resources = toolkitParser.getResources()
+      uriTemplates = toolkitParser.getUriTemplates()
 
-      router = new ApiKitRouter app, resources
+      for template in uriTemplates
+        do (template) ->
+          regexp = utils.pathRegexp template.uriTemplate, [], false, false
+          template.regexp = regexp
 
-      if req.method == 'GET'
-        router.defaultGetRoute res, req.url
+      template = uriTemplates.filter (template) ->
+        req.url.match(template.regexp)?.length
+
+      if template.length
+        router = new ApiKitRouter app, resources
+
+        if req.method == 'GET'
+          router.defaultGetRoute res, req.url, template[0].uriTemplate
+
+        if req.method == 'POST'
+          router.defaultPostRoute res, req.url, template[0].uriTemplate
+
+        if req.method == 'PUT'
+          router.defaultPutRoute res, req.url, template[0].uriTemplate
+
+        if req.method == 'DELETE'
+          router.defaultDeleteRoute res, req.url, template[0].uriTemplate
 
       next()
 
@@ -42,19 +92,18 @@ app = express()
 
 app.set('port', process.env.PORT || 3000)
 app.use(express.logger('dev'))
-app.use(express.json())
 app.use(express.bodyParser())
 app.use(express.methodOverride())
 
 app.use apiKit('../leagues/leagues.raml')
 
-app.get('/teams', (req, res) ->
-	res.send([{ name: 'All Teams' }])
-)
+# app.get('/teams', (req, res) ->
+# 	res.send([{ name: 'All Teams' }])
+# )
 
-app.get('/teams/:id', (req, res) ->
-  res.send({ name: 'by Id' })
-)
+# app.get('/teams/:id', (req, res) ->
+#   res.send({ name: 'by Id' })
+# )
 
 http.createServer(app).listen(app.get('port'), () ->
   console.log('Express server listening on port ' + app.get('port'))

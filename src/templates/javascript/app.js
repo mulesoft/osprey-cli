ea4ca9719e4254fdcaea8c940851b4108e53ca11
Javier Centurion <javier.centurion@southworks.net>
2013-12-22 19:34:35 -0300

    update coffeescript template

--- src/templates/javascript/app.js
+++ src/templates/javascript/app.js
@@ -9,17 +9,6 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.compress());
 
-// APIKit Configuration
-app.use(apiKit(__dirname + '/assets/raml/api.raml', app.routes));
-
-app.use('/api/console', express.static(__dirname + '/assets/console'));
-
-// TODO: This should be move to the apikit runtime
-app.get('/api', function(req, res) {
-  if (/application\/raml\+yaml/.test(req.headers['accept']))
-    res.sendfile(__dirname + '/assets/raml/api.raml');
-  else
-    res.send(415);
-});
+apiKit.register('/api', app, __dirname);
 
 http.createServer(app).listen(3000);
